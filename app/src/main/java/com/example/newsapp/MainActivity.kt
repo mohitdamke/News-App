2813eec742f62a682ff053200ed8d788c72ced68
raees <mohitdamke1@gmail.com>
2024-06-29 19:57:03 +0530

    HomeFile

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    mainScreen(it)
+                }
+            }
+        }
+    }
+
+    private fun mainScreen(it: PaddingValues) {
+
+
+    }
+}

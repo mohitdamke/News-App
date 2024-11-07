d39e3a78138da98c7c94d4577521caddf21aa3cb
raees <mohitdamke1@gmail.com>
2024-11-07 15:00:46 +0530

    Config Changes

--- app/src/main/java/com/example/newsapp/domain/usecases/app_entry/SaveAppEntry.kt
+++ app/src/main/java/com/example/newsapp/domain/usecases/app_entry/SaveAppEntry.kt
@@ -1,6 +1,5 @@
 package com.example.newsapp.domain.usecases.app_entry
 
-import com.example.newsapp.data.manager.LocalUserManagerImpl
 import com.example.newsapp.domain.manager.LocalUserManager
 
 class SaveAppEntry(
